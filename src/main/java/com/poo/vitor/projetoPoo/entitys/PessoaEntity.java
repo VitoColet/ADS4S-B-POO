01b2e0cd2baac6c850a21922a7b7a6a74f82c0c1
Your Name <your_email@example.com>
2025-08-28 21:28:17 -0300

    primeiro commit

---
+++ src/main/java/com/poo/vitor/projetoPoo/entitys/PessoaEntity.java
@@ -0,0 +1,9 @@
+package com.poo.vitor.projetoPoo.entitys;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "pessoa")
+public class PessoaEntity {
+}
