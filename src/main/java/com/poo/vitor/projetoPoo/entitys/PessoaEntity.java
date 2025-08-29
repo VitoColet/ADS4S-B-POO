e38729899868c8b1c5585fbb2eed80c7d39aabca
Your Name <your_email@example.com>
2025-08-28 21:35:42 -0300

    apagando alterações

--- src/main/java/com/poo/vitor/projetoPoo/entitys/PessoaEntity.java
+++
@@ -1,9 +0,0 @@
-package com.poo.vitor.projetoPoo.entitys;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-
-@Entity
-@Table (name = "pessoa")
-public class PessoaEntity {
-}
